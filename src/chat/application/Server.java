9fcb73d22bf6e4628193a57dec5bc19037dff5c8
Vivek <tvivek966@gmail.com>
2025-06-26 21:18:22 +0530

    Added textarea

--- src/chat/application/Server.java
+++ src/chat/application/Server.java
@@ -3,58 +3,66 @@ package chat.application;
 import java.awt.*;
 import javax.swing.*;
 
+
 public class Server extends JFrame {
 
+    private static final Color MAIN_BACKGROUND_COLOR = new Color(84, 84, 84);
+    private static final Color HEADER_BACKGROUND_COLOR = new Color(50, 50, 50);
+    private static final Color FOOTER_BACKGROUND_COLOR = new Color(50, 50, 50);
+
     Server() {
-        setLayout(new BorderLayout());
+        setLayout(null); // layout for the frame
 
-        ImageIcon icon_image = new ImageIcon("src\\assets\\icons\\icon_header.png");
-        setIconImage(icon_image.getImage());
-        ImageIcon send_image = new ImageIcon("src\\assets\\icons\\send_image.png");
+        setIconImage(new ImageIcon("src\\assets\\icons\\icon_header.png").getImage());
 
-        Image img = send_image.getImage();
-        Image resizedImage = img.getScaledInstance(35, 35, Image.SCALE_SMOOTH);
+        ImageIcon sendImageIcon = new ImageIcon("src\\assets\\icons\\send_image.png");
+        Image resizedImage = sendImageIcon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
         ImageIcon resizedIcon = new ImageIcon(resizedImage);
 
 
-        JLabel heading_label = new JLabel();
-        heading_label.setText("Chat Away");
-        heading_label.setForeground(Color.WHITE);
-        heading_label.setHorizontalAlignment(JLabel.CENTER);
+        // Create heading label
+        JLabel headingLabel = new JLabel("Chat Away", JLabel.CENTER);
+        headingLabel.setForeground(Color.WHITE);
 
-        JLabel send_label = new JLabel(resizedIcon);
-        send_label.setBounds(540, 8, 40, 35); 
+        // Create send button icon label
+        JLabel sendLabel = new JLabel(resizedIcon);
 
 
-        JPanel p1 = new JPanel();
-        p1.setBackground(new Color(50, 50, 50));
-        p1.setPreferredSize(new Dimension(getWidth(), 30)); 
-        add(p1, BorderLayout.NORTH);
+        JPanel p1 = createPanel(HEADER_BACKGROUND_COLOR, 0, 0, 600, 25);
+        JPanel p2 = createPanel(FOOTER_BACKGROUND_COLOR, 0, 420, 600, 40);
 
-        JPanel p2 = new JPanel();
-        p2.setBackground(new Color(60, 60, 60));
-        p2.setPreferredSize(new Dimension(getWidth(), 50)); 
-        p2.add(send_label);
-        p2.setLayout(null);
-        add(p2, BorderLayout.SOUTH);
+        JTextField text = new JTextField();
+        text.setBounds(20, 430, 560, 20);
 
 
 
+        p2.setLayout(new FlowLayout(FlowLayout.RIGHT, 15, 15));
+        p2.add(sendLabel);
 
+        // Add heading label to p1
+        p1.add(headingLabel);
 
-        p1.add(heading_label);
+        // Add panels to frame
+        add(p1);
+        add(p2);
+        p2.add(text);
 
+        // Frame properties
         setSize(600, 500);
         setLocation(200, 50);
         setTitle("Chat Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-
-        getContentPane().setBackground(new Color(84, 84, 84));
+        getContentPane().setBackground(MAIN_BACKGROUND_COLOR);
 
         setVisible(true);
+    }
+
+    private JPanel createPanel(Color color, int x_cor, int y_cor, int width, int height) {
+        JPanel panel = new JPanel();
+        panel.setBackground(color);
+        panel.setBounds(x_cor, y_cor, width, height);
+        panel.setPreferredSize(new Dimension(width, height));
+        return panel;
     }
 
     public static void main(String[] args){
