d430a8ebc3d9c106d837a633cc3e41130ae0ca5e
Vivek <tvivek966@gmail.com>
2025-06-27 08:57:50 +0530

    Implemented Text Visblity on hero section

--- src/chat/application/Server.java
+++ src/chat/application/Server.java
@@ -4,12 +4,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class Server extends JFrame implements ActionListener{
 
     private static final Color MAIN_BACKGROUND_COLOR = new Color(84, 84, 84);
     private static final Color HEADER_BACKGROUND_COLOR = new Color(50, 50, 50);
-    private static final Color HERO_BACKGROUND_COLOR = new Color(120, 120, 120, 120);
+    private static final Color CHAT_BOX_COLOR = new Color(255,253,	208);
     private static final Color FOOTER_BACKGROUND_COLOR = new Color(50, 50, 50);
 
     JTextField text;
@@ -33,7 +34,7 @@ public class Server extends JFrame implements ActionListener{
         // Create and position panels
         JPanel p1 = createPanel(HEADER_BACKGROUND_COLOR, 0, 0, 600, 25);
         JPanel p2 = createPanel(FOOTER_BACKGROUND_COLOR, 0, 420, 600, 40);
-        p3 = createPanel(HERO_BACKGROUND_COLOR, 0, 25, 590, 395);
+        p3 = createPanel(MAIN_BACKGROUND_COLOR, 0, 30, 583, 395);
 
         // Positioning text field correctly
         text = new JTextField();
@@ -48,7 +49,6 @@ public class Server extends JFrame implements ActionListener{
         p3.setOpaque(false);
 
         add(p1);
-
         add(p3);
         add(text);
         add(sendButton);
@@ -77,29 +77,57 @@ public class Server extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         String output = text.getText();
 
-        JLabel output_label = new JLabel(output);
-        Font chat_font = new Font("Arial",Font.PLAIN, 16);
-        output_label.setFont(chat_font);
-        output_label.setForeground(Color.white);
+        JPanel local_panel = formatLabel(output);
 
-        JPanel local_panel = new JPanel();
-        local_panel.add(output_label);
-        local_panel.setBackground(MAIN_BACKGROUND_COLOR);
-        
         p3.setLayout(new BorderLayout());
 
         JPanel right = new JPanel(new BorderLayout());
         right.add(local_panel, BorderLayout.LINE_END);
+        right.setOpaque(false);
+
+
         vertical.add(right);
         vertical.add(Box.createVerticalStrut(20));
+        vertical.setOpaque(false);
 
         p3.add(vertical, BorderLayout.PAGE_START);
 
 
+
+        text.setText("");
         repaint();
         invalidate();
         validate();
 
+    }
+
+    public static JPanel formatLabel(String out){
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+
+        Border chat_box_border = new EmptyBorder(10,10,10,10);   
+        
+        JLabel output = new JLabel(out);
+        output.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        output.setBackground(CHAT_BOX_COLOR);  
+        output.setForeground(Color.black);
+        output.setOpaque(true);
+        output.setBorder(chat_box_border);  
+        String labelText = String.format("<html><div WIDTH=%d>%s</div></html>", 400, out);
+        output.setText(labelText);
+        
+
+        FontMetrics metrics = output.getFontMetrics(output.getFont());
+        int textWidth = metrics.stringWidth(out);
+        int height_req_factor = (textWidth/400)+1;
+
+        
+        System.out.println(textWidth);
+
+        p.add(output);
+        p.setBackground(MAIN_BACKGROUND_COLOR);
+
+        return p;
     }
 
     public static void main(String[] args) {
